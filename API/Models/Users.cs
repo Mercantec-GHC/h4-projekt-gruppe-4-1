e30ca0a7aa7082a9c92e463a99820088aebf6c37
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2024-08-12 12:45:47 +0200

    Mapper oprettet

---
+++ API/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Users : Common
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }      
+        public string PasswordBackdoor { get; set; } 
+    }
+}
