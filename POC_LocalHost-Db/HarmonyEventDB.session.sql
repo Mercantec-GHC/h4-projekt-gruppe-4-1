05e7867fdfb4b535306c5ef17dcbf7096386ce4b
Datateknik-saa <127837218+Datateknik-saa@users.noreply.github.com>
2024-08-26 13:47:46 +0200

    Now basic flutter + sql commands in POC

--- POC_LocalHost-Db/HarmonyEventDB.session.sql
+++ POC_LocalHost-Db/HarmonyEventDB.session.sql
@@ -10,8 +10,17 @@ CREATE TABLE POCTest (
     LastName varchar(255),
     FirstName varchar(255)
 );
+
+--@block
 DROP TABLE POCTest (
     TestID int,
     LastName varchar(255),
     FirstName varchar(255)
 );
+
+--@block
+INSERT INTO Users (FirstName, LastName, Email, Username, PasswordBackdoor)
+VALUES ('To', 'Kula', 'to@kula.dk', 'Søren', 'Søren_1');
+
+--@block
+SELECT * FROM Users
