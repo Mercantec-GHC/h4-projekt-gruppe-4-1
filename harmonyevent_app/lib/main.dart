23e998782de09f908fec537c991e97e56b516ad3
Datateknik-saa <127837218+Datateknik-saa@users.noreply.github.com>
2024-08-27 09:00:26 +0200

    add logo in app

--- harmonyevent_app/lib/main.dart
+++ harmonyevent_app/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         title: 'Harmony Event',
         theme: ThemeData(
           useMaterial3: false,
-          //colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 246, 243, 243)),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 183, 211, 83)),
           scaffoldBackgroundColor: const Color.fromARGB(255, 36, 51, 6),
         ),
         
@@ -29,25 +29,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+
+
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 }
+
+
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
+
     return Scaffold(
       body: Column(
         children: [
           Text('How to style this?'),
           Text(appState.current.asLowerCase),
-
+          Image(image: AssetImage('assets/images/HE_Logo.png')),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+              
+            },
+            child: Text('Next >'),
+          
+          ),
         ],
         
+        
       ),
+      
     );
   }
 }
+
+
+
+
 
