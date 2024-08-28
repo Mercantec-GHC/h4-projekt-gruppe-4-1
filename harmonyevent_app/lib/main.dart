bc4de297186e55bdbd6bddfde3ca29a5f67c2afc
Datateknik-saa <127837218+Datateknik-saa@users.noreply.github.com>
2024-08-28 12:00:15 +0200

    + div styling

--- harmonyevent_app/lib/main.dart
+++ harmonyevent_app/lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:english_words/english_words.dart';
+//import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_gradient_button/flutter_gradient_button.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(    
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Harmony Event',
+        //title: 'Harmony Event',
         theme: ThemeData(
           useMaterial3: false,
           colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 183, 211, 83)),
@@ -29,77 +28,126 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
-
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
+  //var current = WordPair.random()
     void getNext() {
-    current = WordPair.random();
+    //current = WordPair.random();
     notifyListeners();
-  }
+  } 
 }
-
-
 
 class MyHomePage extends StatelessWidget {
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
+    //var appState = context.watch<MyAppState>();
+    //var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('How to style this?'),
-          BigCard(pair: pair),
-          Image(image: AssetImage('assets/images/HE_Logo.png')),
-          Padding(  
-            padding: const EdgeInsets.all(15),
-            child: Text('Harmony Event'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-              
-            },
-            child: Text('Next >'),
-          ),
-
-        ],
-        
-        
+      body:  Center(
+        child: Column(
+          children: [
+            Padding(  
+              padding: const EdgeInsets.all(35),
+            ),
+            //BigCard(pair: pair),
+            Image(image: AssetImage('assets/images/HE_Logo.png'),
+              //height: 400,
+              width: 350,
+              fit: BoxFit.cover
+            ),
+            Padding(  
+              padding: const EdgeInsets.all(35),
+              child: LogoText(),
+            ),
+            GradientButton(
+              colors: [const Color.fromARGB(255, 183, 211, 54), const Color.fromARGB(255, 109, 190, 66)],
+              height: 40,
+              width: 300,
+              radius: 20,
+              gradientDirection: GradientDirection.leftToRight,
+              textStyle: TextStyle(color: const Color.fromARGB(255, 234, 208, 225)),
+              text: "Login",
+                onPressed: () {
+                print("Button clicked");
+              },
+            ),
+            Padding(  
+              padding: const EdgeInsets.all(15),
+            ),
+            GradientButton(
+              colors: [const Color.fromARGB(255, 183, 211, 54), const Color.fromARGB(255, 109, 190, 66)],
+              height: 40,
+              width: 300,
+              radius: 20,
+              gradientDirection: GradientDirection.leftToRight,
+              textStyle: TextStyle(color: const Color.fromARGB(255, 234, 208, 225)),
+              text: "Create User",
+              onPressed: () {
+                print("Button clicked");
+              },
+            ),
+          ],
+        ),
       ),
-      
-    );
+    ); 
   }
 }
 
-class BigCard extends StatelessWidget {
-  const BigCard({
+class LogoText extends StatelessWidget {
+  const LogoText({
     super.key,
-    required this.pair,
   });
 
-  final WordPair pair;
+  final txt = "Harmony Event";
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
+      var theme = Theme.of(context);
+      var style = theme.textTheme.displaySmall!.copyWith(
+      color: const Color.fromARGB(255, 234, 208, 225),
     );
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(pair.asLowerCase, style: style),
-      ),
-    );
+    return Text(txt, style: style);
   }
 }
+
+// class MainButtonText extends StatelessWidget {
+//   const MainButtonText({
+//     super.key,
+//   });
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(15),
+//       child: Text('Next >'),
+//     );   
+//   }
+// }
+
+// class BigCard extends StatelessWidget {
+//   const BigCard({
+//     super.key,
+//     required this.pair,
+//   });
+
+//   final WordPair pair;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var theme = Theme.of(context);
+//     var style = theme.textTheme.displayMedium!.copyWith(
+//       color: theme.colorScheme.onPrimary,
+//     );
+//     return Card(
+//       color: theme.colorScheme.primary,
+//       child: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Text(pair.asLowerCase, style: style),
+//       ),
+//     );
+//   }
+// }
 
 
 
