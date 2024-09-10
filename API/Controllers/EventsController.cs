57d998070a0f2671b77ced5af404ab2e7a7f6496
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2024-09-10 09:57:51 +0200

    Update EventsController.cs

--- API/Controllers/EventsController.cs
+++ API/Controllers/EventsController.cs
@@ -1,6 +1,6 @@
-﻿using API.Context;
+using API.Context;
 using API.Models;
-using Google.Protobuf.WellKnownTypes;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +11,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
+using API.Service;
 
 
 namespace API.Controllers
@@ -21,11 +22,16 @@ namespace API.Controllers
     {
         private readonly AppDBContext _dbContext;
         private readonly IConfiguration _configuration;
-
-        public EventController(AppDBContext dbContext, IConfiguration configuration)
+        private readonly string _accessKey;
+        private readonly string _secretKey;
+        private readonly R2Service _r2Service;
+        public EventController(AppDBContext dbContext, IConfiguration configuration, AppConfiguration config)
         {
             _dbContext = dbContext;
             _configuration = configuration;
+            _accessKey = config.AccessKey;
+            _secretKey = config.SecretKey;
+            _r2Service = new R2Service(_accessKey, _secretKey);
         }
 
         [HttpGet]
@@ -37,8 +43,10 @@ namespace API.Controllers
                     Id = events.id,
                     Date = events.Date,
                     Place_id = events.Place_id,
+                    ImageURL = events.EventPictureURL,
                     Description = events.Description,
-                    Category = events.Category
+                    Category = events.Category,
+                    isprivate = events.isprivate,
                 })
                 .ToListAsync();
 
@@ -64,45 +72,48 @@ namespace API.Controllers
 
 
         [HttpPost("Create")]
-        public async Task<ActionResult<Event>> PostEvent(CreateEventDTO eventCreate)
+        public async Task<IActionResult> PostUser([FromForm] CreateEventDTO eventCreate)
         {
 
             var events = new Event
             {
                 id = Guid.NewGuid().ToString("N"),
                 Date = eventCreate.Date,
-                
-                Place_id= eventCreate.Place_id,
-                ImageURL = eventCreate.ImageURL,
-                Type = eventCreate.Type,
+
+                Place_id = eventCreate.Place_id,
+             
+                isprivate = eventCreate.isprivate,
                 Description = eventCreate.Description,
-                
+
                 Category = eventCreate.Category,
-                
-               
+
+
 
             };
 
 
-            _dbContext.Events.Add(events);
+           
+            var r2Service = new R2Service(_accessKey, _secretKey);
+            var imageUrl = await r2Service.UploadToR2(eventCreate.EventPicture.OpenReadStream(), "PP" + events.id);
 
+            events.EventPictureURL = imageUrl;
+            _dbContext.Events.Add(events);
             try
             {
-              
+
                 await _dbContext.SaveChangesAsync();
             }
             catch (DbUpdateException ex)
             {
-                
+
                 return StatusCode(500, new { message = "An error occurred while saving the event.", details = ex.Message });
             }
 
-            
+
             return CreatedAtAction(nameof(GetEventById), new { id = events.id }, events);
         }
 
-     
+
     }
 }
 
-        
