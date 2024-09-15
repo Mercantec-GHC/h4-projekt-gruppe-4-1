6ed6d1d42f4b7cf9dd39009597e761631b988dac
Datateknik-saa <127837218+Datateknik-saa@users.noreply.github.com>
2024-09-15 14:56:56 +0200

    SOC - added custom appbar in new components folder

--- harmonyevent_app/lib/pages/EventPage.dart
+++ harmonyevent_app/lib/pages/EventPage.dart
@@ -1,14 +1,15 @@
 
 import 'package:flutter/material.dart';
-
 import 'package:flutter_gradient_button/flutter_gradient_button.dart';
 //import 'package:flutter_icon_shadow/flutter_icon_shadow.dart';
 
 import 'package:harmonyevent_app/models/event_model.dart';
 import 'package:harmonyevent_app/services/fetch_service.dart';
 
-import 'package:harmonyevent_app/pages/CreateEventPage.dart';
-import 'package:harmonyevent_app/pages/UserProfilePage.dart';
+// import 'package:harmonyevent_app/pages/CreateEventPage.dart';
+// import 'package:harmonyevent_app/pages/UserProfilePage.dart';
+import 'package:harmonyevent_app/components/custom_appbar.dart';
+
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -29,99 +30,8 @@ class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {   
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        actions: <Widget>[
-
-          //SEARCH EVENTS
-          IconButton(
-            icon: const Icon(
-              Icons.search, 
-              color: Color.fromARGB(255, 183, 211, 54),
-                         size: 26,
-              ),
-            tooltip: "Search Events",
-            onPressed: () {
-              print("Hmm");
-            }
-          ),
-          //ALL EVENTS
-          IconButton(
-            icon: Icon(
-              Icons.event, 
-              color: const Color.fromARGB(255, 234, 208, 225),
-            ),
-            tooltip: "All Events",
-            onPressed: () {
-              Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => EventPage()), // Replace with the correct page
-              );
-            }
-          ),
-          //CREATE EVENT
-          IconButton(
-            icon: Icon(
-              Icons.library_add_outlined, 
-              color: const Color.fromARGB(255, 183, 211, 54),
-            ),
-            tooltip: "Create Event",
-            onPressed: () {
-              Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => CreateEventPage()), // Replace with the correct page
-              );
-            }
-          ),
-          //PLANNED EVENTS
-          IconButton(
-            icon: const Icon(
-              Icons.event_available, 
-              color: Color.fromARGB(255, 183, 211, 54)
-              ),
-            tooltip: "Planned Events",
-            onPressed: () {
-              print("Hmm");
-            }
-          ),
-
-          //USER PROFILE
-          IconButton(
-            icon: const Icon(
-              Icons.manage_accounts_sharp, 
-              color: Color.fromARGB(255, 183, 211, 54),
-              size: 26
-            ),
-            tooltip: "Your account",
-            onPressed: () {
-              Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => UserProfilePage()), // Replace with the correct page
-              );
-            }
-          ),
-        ],
-        title: Row(
-          children: [
-            Container(
-              child: Image(image: AssetImage('assets/images/HE_Logo.png'),
-                width: 50,
-                fit: BoxFit.cover     
-              ),  
-            ),        
-            Container(          
-              child: 
-              Text(
-                "Harmony Event",
-                style: TextStyle(
-                  color: const Color.fromARGB(255, 234, 208, 225),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),  
-            ), 
-          ],
-        ),
-      ),
+      appBar: customAppBar(),
+      
       body: Center(
         child: FutureBuilder<List<EventDTO>>(
           future: eventsFuture,
@@ -326,3 +236,4 @@ class _EventPageState extends State<EventPage> {
     );
   }
 }
+
