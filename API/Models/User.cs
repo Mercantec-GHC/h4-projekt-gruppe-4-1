0026cf4e955a9a379b2a4aa2ce4cdbee92bb6f32
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2024-09-16 16:37:56 +0200

    yea

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -31,10 +31,16 @@ namespace API.Models
     }
     public class UserDTO
     {
+        public String? FirstName { get; set; }
+        public String? LastName { get; set; }
         public String Id { get; set; }
         public String Email { get; set; }
         public String Username { get; set; }
         public String ProfilePicture { get; set; }
+        public String? Address { get; set; }
+        public String? Postal { get; set; }
+        public String? City { get; set; }
+
     }
 
     public class LoginDTO
