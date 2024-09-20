05a7ef38ef5c2596c7a0cef6c223ce9ad2fe2b45
Datateknik-saa <127837218+Datateknik-saa@users.noreply.github.com>
2024-09-20 08:40:20 +0200

    ????

--- harmonyevent_api/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ harmonyevent_api/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6bd16ec7846b3ed8a6a612f2ebd9815eb78585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
